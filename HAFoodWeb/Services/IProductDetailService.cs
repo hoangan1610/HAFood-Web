aee710c173f9228f63f277671837033a511dc38a
hoangan1610 <ngohoangan0@gmail.com>
2025-10-18 18:02:42 +0700

    Create: ProductDetail

---
+++ HAFoodWeb/Services/IProductDetailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using HAFoodWeb.Models;
+
+namespace HAFoodWeb.Services
+{
+    public interface IProductDetailService
+    {
+        Task<ProductDetailDto> GetProductDetailAsync(long id);
+    }
+}
